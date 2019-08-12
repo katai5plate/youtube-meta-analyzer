38c79dc50905cafe05a12581d95c667a1b47231c
katai5plate <katai5plate.sub@gmail.com>
2019-08-12 19:54:56 +0900

    add category & atJP

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
+const INPUT = require('./input.json');
+const { key: KEY } = require('./config.json');
 const YouTube = require('youtube-node');
 const fs = require('fs-extra');
-const input = require('./input.json');
+const moment = require('moment');
+const req = require('request');
+
+moment.locale('ja');
 
 (async () => {
   const y = new YouTube();
-  y.setKey(require('./config.json').key);
+  y.setKey(KEY);
 
   const sleep = ms => new Promise(f => setTimeout(f, ms));
+  const getCategories = () =>
+    new Promise((ok, ng) =>
+      req.get(
+        `https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=jp&key=${KEY}`,
+        (e, r) => (e ? ng(e) : ok(r))
+      )
+    );
   const getVideoData = id =>
     new Promise((ok, ng) => y.getById(id, (e, r) => (e ? ng(e) : ok(r))));
 
-  let results = [],
-    res = {};
-  for (let id of input) {
+  let CATEGORIES = [];
+  try {
+    CATEGORIES = JSON.parse((await getCategories()).body).items;
+    console.log('DONE FETCH CATEGORIES');
+  } catch (error) {
+    console.log('FETCH CATEGORIES ERROR', error);
+    return;
+  }
+
+  let results = [];
+  let res = {};
+
+  for (let id of INPUT) {
     for (let timeout = 0; timeout < 5; timeout++) {
       if (timeout === 0) {
         console.log('READY', id);
@@ -28,13 +50,25 @@ const input = require('./input.json');
         console.log('TIMEOUT', id, e);
       }
     }
+    const {
+      title,
+      channelTitle: author,
+      publishedAt,
+      categoryId,
+      tags
+    } = res.items[0].snippet;
     results = [
       ...results,
       {
-        id: res.items[0].id,
-        title: res.items[0].snippet.title,
-        at: res.items[0].snippet.publishedAt,
-        tags: res.items[0].snippet.tags
+        id,
+        title,
+        author,
+        at: moment(publishedAt).toISOString(),
+        atJP: moment(publishedAt).format('YYYY年MM月DD日(dd) HH時mm分ss秒'),
+        categoryId,
+        categoryName: CATEGORIES.filter(({ id: cid }) => cid === categoryId)[0]
+          .snippet.title,
+        tags: tags
       }
     ];
   }
